c678b7e13aba95962119fe484c06c2eb1ca6f405
ParkerHiggins80 <higgins.par@northeastern.edu>
2026-01-21 12:49:22 -0500

    Add assignment files

---
+++ MiniProject1_Higgins.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+
+double add (double a, double b)
+{
+    return a+b;
+}
+
+double subtract (double a, double b)
+{
+    return a-b;
+}
+
+double multiply (double a, double b)
+{
+    return a*b;
+}
+
+double divide (double a, double b)
+{
+    return a/b;
+}
+
+double mod (int a, int b)
+{
+    return a%b;
+}
+    
+
+int main() 
+{
+    int choice;
+
+    do
+    {
+        
+        cout << "Calculator" << endl;
+        cout << "1: Add 2 Doubles" << endl;        
+        cout << "2: Subtract 2 Doubles" << endl;
+        cout << "3: Multiply 2 Doubles" << endl;
+        cout << "4: Divide 2 Doubles" << endl;
+        cout << "5: Module 2 Integers" << endl;
+        cout << "0: Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cout <<"\n"<<endl;
+
+            switch(choice)
+            {
+                case 0:
+                {
+                    break;
+                }
+                case 1:
+                {
+                    double x,y;
+                    cout <<"Enter first double: ";
+                    cin >> x;
+                    cout <<"Enter second double: ";
+                    cin >> y;
+
+                    cout<<x<<" + "<<y<<" = "<<add(x,y) <<endl;
+                    cout <<endl;
+                    break;
+                }
+                case 2:
+                {
+                    double x,y;
+                    cout <<"Enter first double: ";
+                    cin >> x;
+                    cout <<"Enter second double: ";
+                    cin >> y;
+
+                    cout<<x<<" - "<<y<<" = "<<subtract(x,y) <<endl;
+                    cout <<endl;
+                    break;
+                }
+                case 3:
+                {
+                    double x,y;
+                    cout <<"Enter first double: ";
+                    cin >> x;
+                    cout <<"Enter second double: ";
+                    cin >> y;
+
+                    cout<<x<<" * "<<y<<" = "<<multiply(x,y) <<endl;
+                    cout <<endl;
+                    break;
+                }
+                case 4:
+                {
+                    double x,y;
+                    cout <<"Enter a double: ";
+                    cin >> x;
+                    cout <<"Enter an integer: ";
+                    cin >> y;
+
+                    cout<<x<<" / "<<y<<" = "<<divide(x,y) <<endl;
+                    cout <<endl;
+                    break;
+                }
+                case 5:
+                {
+                    double x,y;
+                    cout <<"Enter first integer: ";
+                    cin >> x;
+                    cout <<"Enter second integer: ";
+                    cin >> y;
+
+                    cout<<x<<" % "<<y<<" = "<<divide(x,y) <<endl;
+                    cout <<endl;
+                    break;
+                }
+            }
+
+    }
+    while(choice!=0);
+    return 0;
+    
+}
